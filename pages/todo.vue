95c2dd420e3a529731f2a3f9a59174a26cd5e575
Naral <77269201+NaralC@users.noreply.github.com>
2023-08-29 00:46:49 +0100

    tasks display

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,6 +1,34 @@
 <script lang="ts" setup>
+const tasks = ref([
+  {
+    id: 1,
+    title: "Wake up",
+  },
+  {
+    id: 2,
+    title: "Get milk",
+  },
+  {
+    id: 3,
+    title: "Drink milk",
+  },
+]);
 </script>
 
 <template>
-  <div class="pa-5">home page</div>
+  <div class="">
+    <v-list v-for="task in tasks" lines="one" select-strategy="classic">
+      <v-list-item :key="task.id" :value="task.title">
+        <template v-slot:prepend="{ isActive }">
+          <v-list-item-action start>
+            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+          </v-list-item-action>
+        </template>
+
+        <v-list-item-title>{{ task.title }}</v-list-item-title>
+      </v-list-item>
+
+      <v-divider></v-divider>
+    </v-list>
+  </div>
 </template>
